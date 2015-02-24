d60c637635915ec75cdfad0d8ee4ce4074ca5eb7
Alex Sorokine <sorokine@gmail.com>
2015-02-23 22:59:09 -0500

    1st working version

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ exports.init = function( address, bus, callback ) {
                     else
                         _sensor_ver = res;
                 }
-            }
+            });
         },
         
         /* 0x00 = ENABLE register */
@@ -40,60 +40,56 @@ exports.init = function( address, bus, callback ) {
         /* 0x01 = Power on, 0x02 RGB sensors enabled */
         function(cb) {
             _wire.writeByte(0x01|0x02, function(err) { cb(err) });
-        },
+        }
         
-
-    ], function(err) {
-        if (err) callback(err);
+    ], 
+    function(err) { 
+        if (err) callback(err); 
     });
+    
+    return true;
 }
 
 exports.readRaw = function (callback) {
-    wire.readBytes(0x80|0x14, 8, function(err, data) {
-        if (callback)
-            callback(err, data);
-        else
-            return data
-    }
+    _wire.readBytes(0x80|0x14, 8, function(err, data) {
+        callback && callback(err, data);
+        return data;
+    });
 }
 
 exports.readCRGB = function (callback) {
-    readRaw( function(err, data) {
-
-        if (err) {
+    exports.readRaw( function(err, data) {
+        if (err) 
             callback && callback(err, null);
         else {
             var clear = clear = data[1] << 8 | data[0];
             var red = data[3] << 8 | data[2];
             var green = data[5] << 8 | data[4];
             var blue = data[7] << 8 | data[6];
-            
-            if (callback)
-                callback(err, [clear, red, green, blue]);
-            else
-                return [clear, red, green, blue];
+
+            var arr = [clear, red, green, blue];
+            callback && callback(err, arr);
+            return arr;
         }
     });
 }
 
 exports.read2string = function (callback) {
-    readCRGB( function(err, crgb) ) {
-        if (err) {
+    exports.readCRGB( function(err, crgb) {
+        if (err) 
             callback && callback(err, null);
         else {
             var crgbstr = util.format("C: %s, R: %s, G: %s, B: %s", crgb);
-            if (callback)
-                callback( err, crgbstr );
-            else
-                return crgbstr;
+            callback && callback( err, crgbstr );
+            return crgbstr;
         }       
-    }
+    });
 }
 
 
 exports.read2json = function (callback) {
-    readCRGB( function(err, crgb) ) {
-        if (err) {
+    exports.readCRGB( function(err, crgb) {
+        if (err)
             callback && callback(err, null);
         else {
             var crgbstr = {
@@ -102,10 +98,8 @@ exports.read2json = function (callback) {
                 "green" : crgb[2],
                 "blue"  : crgb[3]
             };
-            if (callback)
-                callback( err, crgbstr );
-            else
-                return crgbstr;
+            callback && callback( err, crgbstr );
+            return crgbstr;
         }       
-    }
+    });
 }
